b61760bef042b73cb76a9be5979a04f349c287ab
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-02 10:28:29 -0600

    Get value of search input

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,19 +5,24 @@ class SearchForm extends Component {
   constructor() {
     super();
     this.state = {
-
+      searchField: ''
     }
   };
 
-  handleSearch(event) {
+  handleChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+  handleSearch = (event) => {
     event.preventDefault()
-    console.log('in handleSearch***')
+    console.log('in handleSearch***', this.state.searchField)
+    
   }
 
   render() {
     return (
       <form>
-        <input type="text"/>
+        <input type="text" placeholder="Search" onChange={event => this.handleChange(event)}/>
         <button onClick={this.handleSearch}>Submit Search</button>
       </form>
     )
