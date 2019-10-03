ee2aad4d2ce8ab78e84c6678648364f0b7a2781b
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-03 17:55:07 -0600

    Set up and pass initial testing for Menu

---
+++ src/components/Menu/Menu.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Menu from './Menu';
+
+describe('Menu', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<Menu selectTopic='local'/>)
+
+    console.log('WRAP THIS', wrapper)
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+}); //<<------ end of describe block
