3aaa5d6095a19da021fdd3d6687a0e9036b636cc
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-03 18:34:59 -0600

    Get SearchForm testing set up

---
+++ src/components/SearchForm/SearchForm.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SearchForm from './SearchForm';
+
+describe('SearchForm', () => {
+//   it('should match the snapshot with all data passed in correctly', () => {
+//     const wrapper = shallow(<SearchForm search={
+// [{headline="Spider-Man Will Remain in the Marvel Cinematic Universe",
+//       img="https://static01.nyt.com/images/2019/09/27/multimedia/27xp-spiderman/27xp-spiderman-superJumbo.jpg?quality=90&auto=webp",
+//       description="The web slinger will be the only superhero with the ability to cross between Disney and Sony’s cinematic universes, according to Marvel Studios.",
+//       url="https://www.nytimes.com/2019/09/27/business/media/spiderman-mcu-sony-marvel.html"}]
+
+
+
+    // }/>)
+  // });
+
+}); //<<-----end of describe block
